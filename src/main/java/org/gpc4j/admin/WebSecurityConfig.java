7a7245604d24940f76e85c8a8fc2985c348ddf77
Lyle Harris <lyle.harris@gcpay.com>
2022-01-29 13:30:15 -0800

    Update SBA and use Spring Security

--- src/main/java/org/gpc4j/admin/WebSecurityConfig.java
+++ src/main/java/org/gpc4j/admin/WebSecurityConfig.java
@@ -1,10 +1,24 @@
 package org.gpc4j.admin;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@Configuration
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-public class WebSecurityConfig {
+import java.util.UUID;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final AdminServerProperties adminServer;
 
@@ -12,41 +26,41 @@ public class WebSecurityConfig {
     this.adminServer = adminServer;
   }
 
-//  @Override
-//  protected void configure(HttpSecurity http) throws Exception {
-//    SavedRequestAwareAuthenticationSuccessHandler successHandler =
-//        new SavedRequestAwareAuthenticationSuccessHandler();
-//    successHandler.setTargetUrlParameter("redirectTo");
-//    successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
-//
-//    http
-//        .authorizeRequests()
-//        .antMatchers(this.adminServer.getContextPath() + "/assets/**").permitAll()
-//        .antMatchers(this.adminServer.getContextPath() + "/login").permitAll()
-//        .anyRequest().authenticated()
-//        .and()
-//        .formLogin()
-//        .loginPage(this.adminServer.getContextPath() + "/login")
-//        .successHandler(successHandler)
-//        .and()
-//        .logout()
-//        .logoutUrl(this.adminServer.getContextPath() + "/logout")
-//        .and()
-//        .httpBasic()
-//        .and()
-//        .csrf()
-//        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//        .ignoringRequestMatchers(
-//            new AntPathRequestMatcher(this.adminServer.getContextPath() +
-//                "/instances", HttpMethod.POST.toString()),
-//            new AntPathRequestMatcher(this.adminServer.getContextPath() +
-//                "/instances/*", HttpMethod.DELETE.toString()),
-//            new AntPathRequestMatcher(this.adminServer.getContextPath() + "/actuator/**"))
-//        .and()
-//        .rememberMe()
-//        .key(UUID.randomUUID().toString())
-//        .tokenValiditySeconds(1209600);
-//  }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    SavedRequestAwareAuthenticationSuccessHandler successHandler =
+        new SavedRequestAwareAuthenticationSuccessHandler();
+    successHandler.setTargetUrlParameter("redirectTo");
+    successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
+
+    http
+        .authorizeRequests()
+        .antMatchers(this.adminServer.getContextPath() + "/assets/**").permitAll()
+        .antMatchers(this.adminServer.getContextPath() + "/login").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+        .loginPage(this.adminServer.getContextPath() + "/login")
+        .successHandler(successHandler)
+        .and()
+        .logout()
+        .logoutUrl(this.adminServer.getContextPath() + "/logout")
+        .and()
+        .httpBasic()
+        .and()
+        .csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .ignoringRequestMatchers(
+            new AntPathRequestMatcher(this.adminServer.getContextPath() +
+                "/instances", HttpMethod.POST.toString()),
+            new AntPathRequestMatcher(this.adminServer.getContextPath() +
+                "/instances/*", HttpMethod.DELETE.toString()),
+            new AntPathRequestMatcher(this.adminServer.getContextPath() + "/actuator/**"))
+        .and()
+        .rememberMe()
+        .key(UUID.randomUUID().toString())
+        .tokenValiditySeconds(1209600);
+  }
 
 //
 //  @Bean
